4d842735423cd5dd9c57eab9575b9e687ae77cf4
Wihogora Wivine <wwihogora@gmail.com>
2019-02-28 09:43:32 +0200

    .

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from . import login_manager
 from flask_login import UserMixin
 from datetime import datetime
-# from . import login_manager
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -125,51 +125,5 @@ class Comment(db.Model):
 
 
 
-
-
-# class Movie:
-#     '''
-#     Movie class to define Movie Objects
-#     '''
-
-#     def __init__(self,id,title,overview,poster,vote_average,vote_count):
-#         self.id =id
-#         self.title = title
-#         self.overview = overview
-#         self.poster = "https://image.tmdb.org/t/p/w500/" + poster
-#         self.vote_average = vote_average
-#         self.vote_count = vote_count
-
-
-
-# class Review:
-
-#     all_reviews = []
-
-#     def __init__(self,movie_id,title,imageurl,review):
-#         self.movie_id = movie_id
-#         self.title = title
-#         self.imageurl = imageurl
-#         self.review = review
-
-
-#     def save_review(self):
-#         Review.all_reviews.append(self)
-
-
-#     @classmethod
-#     def clear_reviews(cls):
-#         Review.all_reviews.clear()
-
-#     @classmethod
-#     def get_reviews(cls,id):
-
-#         response = []
-
-#         for review in cls.all_reviews:
-#             if review.movie_id == id:
-#                 response.append(review)
-
-#         return response
 
 
