0d2f467e4ebd287194d54f5133053c159d59941d
Wihogora Wivine <wwihogora@gmail.com>
2019-03-01 18:26:27 +0200

    Add relationship in users table

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from . import login_manager
 from flask_login import UserMixin
+from . import login_manager
 from datetime import datetime
 
 
@@ -18,8 +19,11 @@ class User(UserMixin,db.Model):
     email = db.Column(db.String(255),unique = True,index = True)
     password_hash = db.Column(db.String(255))
     bio = db.Column(db.String(255))
+    subscription = db.Column(db.Boolean)
     profile_pic_path = db.Column(db.String())
     pass_secure = db.Column(db.String(255))
+    date_joined = db.Column(db.DateTime, default=datetime.utcnow)
+
     
     pitches = db.relationship('Pitch',backref = 'user',lazy = "dynamic")
     comments = db.relationship('Comment',backref = 'user',lazy = "dynamic")
